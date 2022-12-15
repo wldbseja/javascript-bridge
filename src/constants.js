d27a05ae3da734fa4d0387a611b9e22469404314
yooni <jiyoon0616@naver.com>
2022-12-15 20:38:32 +0900

    style (constants) : 출력 문구 추가

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,15 @@ const PRINT_STRING = {
   INPUT_GAME_RETRY:
     '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   OUTPUT_GAME_RESULT: '\n최종 게임 결과',
-  OUTPUT_RESULT_PHRASES: (result) =>
-    `\n게임 성공 여부: ${result}\n총 시도한 횟수: ${result}`,
+  OUTPUT_RESULT: (result) => `\n게임 성공 여부: ${result}`,
+  OUTPUT_RETRY_COUNT: (retry) => `총 시도한 횟수: ${retry}`,
 };
 
 const ERROR_PRINT_STRING = {
   ERROR_BRIDGE_SIZE: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
   ERROR_USER_INPUT: '[ERROR] 올바른 입력이 아닙니다.',
 };
+
 const GAME_STATUS = {
   UP: 'U',
   DOWN: 'D',
