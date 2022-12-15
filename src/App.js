3c2be07ddcdc6cfdf5c0bf52bb81dbdff47e24e0
yooni <jiyoon0616@naver.com>
2022-12-15 11:31:52 +0900

    feat (App) : 다리 길이 입력 받는 함수 호출 , 값 받기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 
 class App {
   constructor() {}
 
   play() {
     OutputView.printGameStart();
+    InputView.readBridgeSize(this.handlingBridgeSize);
   }
+
+  handlingBridgeSize(size) {}
 }
 
 module.exports = App;
