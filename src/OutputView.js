c25d53edc055e81bf0a1d45c58c9d22211a788b4
yooni <jiyoon0616@naver.com>
2022-12-15 19:38:37 +0900

    feat (OutputView) : 다리 모양 출력 기능

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,7 +2,7 @@
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 const MissionUtils = require('@woowacourse/mission-utils');
-const { PRINT_STRING } = require('./constants');
+const { PRINT_STRING, GAME_STATUS } = require('./constants');
 
 const OutputView = {
   printGameStart() {
@@ -13,7 +13,13 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  printMap(upPatten, downPatten) {
+    const { START, LINE, END } = GAME_STATUS;
+    const upRow = `${START}${[upPatten].join(LINE)}${END}`;
+    const downRow = `${START}${[downPatten].join(LINE)}${END}`;
+    MissionUtils.Console.print(upRow);
+    MissionUtils.Console.print(downRow);
+  },
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
