698bca50b4cacd856d2f1dd17b31f87a657f53ce
yooni <jiyoon0616@naver.com>
2022-12-15 17:06:41 +0900

    feat (BridgeGame) : 사용자 다리 상태 만들고,  결과 값 만드는 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -6,18 +6,33 @@ const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 
 class BridgeGame {
   #bridgeShape;
+  #userMoveArray;
   constructor(size) {
     this.#bridgeShape = BridgeMaker.makeBridge(
       size,
       BridgeRandomNumberGenerator.generate
     );
+    this.#userMoveArray = [];
   }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  move() {}
+  move(userMove) {
+    this.#userMoveArray.push(userMove);
+    let result;
+    if (
+      this.#bridgeShape[this.#userMoveArray.length - 1] !==
+      this.#userMoveArray[this.#userMoveArray.length - 1]
+    ) {
+      result = false;
+    } else {
+      result = true;
+    }
+
+    return [this.#bridgeShape, this.#userMoveArray, result];
+  }
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
