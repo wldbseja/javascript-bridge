48ccc0849fbdd28f069d7bec01832dc64f11c80f
yooni <jiyoon0616@naver.com>
2022-12-15 13:57:27 +0900

    test (Test) : BridgeMaker.makeBridge

--- __tests__/Test.js
+++ __tests__/Test.js
@@ -1,4 +1,5 @@
 const App = require('../src/App');
+const BridgeMaker = require('../src/BridgeMaker');
 
 describe('게임 기능 테스트', () => {
   test('validateBridgeSize', () => {
@@ -8,5 +9,25 @@ describe('게임 기능 테스트', () => {
     expect(() => app.validateBridgeSize(21)).toThrow();
     expect(() => app.validateBridgeSize(3)).not.toThrow();
     expect(() => app.validateBridgeSize(20)).not.toThrow();
+  });
+
+  test('BridgeMaker.makeBridge', () => {
+    let test1 = {
+      makeTest1() {
+        let up = 1;
+        return up;
+      },
+    };
+
+    expect(BridgeMaker.makeBridge(1, test1.makeTest1)).toEqual(['U']);
+
+    let test2 = {
+      makeTest2() {
+        let down = 0;
+        return down;
+      },
+    };
+
+    expect(BridgeMaker.makeBridge(1, test2.makeTest2)).toEqual(['D']);
   });
 });
