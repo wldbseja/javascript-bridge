07e86db25b5ab43257b7a122a872648fe0204730
yooni <jiyoon0616@naver.com>
2022-12-16 11:25:09 +0900

    style (App) : 출력 문구 줄 바꿈 추가

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const PRINT_STRING = {
-  OUTPUT_GAME_START: '다리 건너기 게임을 시작합니다.',
-  INPUT_BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
-  INPUT_MOVE_PICK: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  OUTPUT_GAME_START: '다리 건너기 게임을 시작합니다.\n',
+  INPUT_BRIDGE_SIZE: '다리의 길이를 입력해주세요.\n',
+  INPUT_MOVE_PICK: '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
   INPUT_GAME_RETRY:
-    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+    '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
   OUTPUT_GAME_RESULT: '\n최종 게임 결과',
   OUTPUT_RESULT: (result) => `\n게임 성공 여부: ${result}`,
   OUTPUT_RETRY_COUNT: (retry) => `총 시도한 횟수: ${retry}`,
