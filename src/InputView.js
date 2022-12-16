774405e86fd0440435aee99bcc2239285b88e2bd
yooni <jiyoon0616@naver.com>
2022-12-16 10:55:56 +0900

    feat (InputView) : 다리 길이 입 출력 기능

--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,17 @@
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
+const MissionUtils = require('@woowacourse/mission-utils');
+const { PRINT_STRING } = require('./constants');
+
 const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+
+  readBridgeSize(callback) {
+    MissionUtils.Console.readLine(PRINT_STRING.INPUT_BRIDGE_SIZE, callback);
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
