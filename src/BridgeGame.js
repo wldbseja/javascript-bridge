00ee99ab96e8cac7936c978821d98df0d9c736f2
yooni <jiyoon0616@naver.com>
2022-12-16 11:49:31 +0900

    feat (BridgeGame) :  makeBridge에 값  전달 후,  랜덤 다리 상태 받기

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,8 +1,17 @@
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
+const BridgeMaker = require('./BridgeMaker');
+
 class BridgeGame {
-  constructor(size) {}
+  #randomBridge;
+  constructor(size) {
+    this.#randomBridge = BridgeMaker.makeBridge(
+      size,
+      BridgeRandomNumberGenerator.generate
+    );
+  }
   /**
    * 사용자가 칸을 이동할 때 사용하는 메서드
    * <p>
