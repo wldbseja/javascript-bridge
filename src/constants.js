3292fa7877c3bb6f0b30eff82e997511ea0b908b
yooni <jiyoon0616@naver.com>
2022-12-16 10:40:21 +0900

    feat (constants) : 출력 문구 및 기능 구현에 필요한 const 파일 추가

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+const PRINT_STRING = {
+  OUTPUT_GAME_START: '다리 건너기 게임을 시작합니다.',
+  INPUT_BRIDGE_SIZE: '다리의 길이를 입력해주세요.',
+  INPUT_MOVE_PICK: '이동할 칸을 선택해주세요. (위: U, 아래: D)',
+  INPUT_GAME_RETRY:
+    '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)',
+  OUTPUT_GAME_RESULT: '\n최종 게임 결과',
+  OUTPUT_RESULT: (result) => `\n게임 성공 여부: ${result}`,
+  OUTPUT_RETRY_COUNT: (retry) => `총 시도한 횟수: ${retry}`,
+};
+
+const ERROR_PRINT_STRING = {
+  ERROR_BRIDGE_SIZE: '[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.',
+  ERROR_USER_INPUT: '[ERROR] 올바른 입력이 아닙니다.',
+};
+
+const GAME_ITEM = {
+  UP: 'U',
+  DOWN: 'D',
+  RETRY: 'R',
+  QUIT: 'Q',
+  MIN_BOUND: 3,
+  MAX_BOUND: 20,
+  START: ' [',
+  END: ' ]',
+  LINE: ' | ',
+  POSSIBLE: 'O',
+  IMPOSSIBLE: 'X',
+  SPACE: ' ',
+  WIN: '성공',
+  LOSE: '실패',
+};
+module.exports = { PRINT_STRING, ERROR_PRINT_STRING, GAME_ITEM };
