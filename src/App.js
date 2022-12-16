9e22a1c8319e45d831dd67616fef9f667b909404
yooni <jiyoon0616@naver.com>
2022-12-16 11:06:50 +0900

    feat (App) : 다리 길이 범위 확인 기능

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
+const { GAME_ITEM, ERROR_PRINT_STRING } = require('./constants');
 
 class App {
   constructor() {
     this.handlingBridgeSize = this.handlingBridgeSize.bind(this);
   }
 
-  handlingBridgeSize(size) {}
+  handlingBridgeSize(size) {
+    this.validateRangeSize(size);
+  }
 
   play() {
     OutputView.printGameStart();
     InputView.readBridgeSize(this.handlingBridgeSize);
+  }
+
+  validateRangeSize(size) {
+    if (
+      !(
+        size >= GAME_ITEM.MIN_BOUND &&
+        size <= GAME_ITEM.MAX_BOUND &&
+        size % GAME_ITEM.ONE == GAME_ITEM.ZERO
+      )
+    )
+      throw Error(ERROR_PRINT_STRING.ERROR_BRIDGE_SIZE);
   }
 }
 
